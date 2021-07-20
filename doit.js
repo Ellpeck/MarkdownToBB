6d03ba9940ef690666ab957c0d8be9f6ca3a0942
Ellpeck <me@ellpeck.de>
2021-07-20 05:43:40 +0200

    fixed multiple italic and bold sections being parsed incorrectly

--- doit.js
+++ doit.js
@@ -7,11 +7,11 @@ rules.set(/^### *(.*)/gm, `[h3]$1[/h3]`);
 rules.set(/^## *(.*)/gm, `[h2]$1[/h2]`);
 rules.set(/^# *(.*)/gm, `[h1]$1[/h1]`);
 // formatting
-rules.set(/\*\*(.*)\*\*/g, `[b]$1[/b]`);
-rules.set(/\_\_(.*)\_\_/g, `[b]$1[/b]`);
-rules.set(/\*(.*)\*/g, `[i]$1[/i]`);
-rules.set(/\_(.*)\_/g, `[i]$1[/i]`);
-rules.set(/~~(.*)~~/g, `[strike]$1[/strike]`);
+rules.set(/\*\*([^*]+)\*\*/g, `[b]$1[/b]`);
+rules.set(/\_\_([^_]+)\_\_/g, `[b]$1[/b]`);
+rules.set(/\*([^\]][^*]+)\*/g, `[i]$1[/i]`);
+rules.set(/\_([^_]+)\_/g, `[i]$1[/i]`);
+rules.set(/~~([^~]+)~~/g, `[strike]$1[/strike]`);
 // rules
 rules.set(/^---/gm, `[hr][/hr]`);
 rules.set(/^\*\*\*/gm, `[hr][/hr]`);
